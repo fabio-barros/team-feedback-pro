151d68c1a4a1e9cea4cf6228a91e54f8d29763c9
Aline Limeira <alinesantanalimeira@gmail.com>
2025-11-21 18:31:09 -0300

    components: btn, cards, status, tags, icons

--- src/TeamFeedbackPro.Front/src/main.tsx
+++ src/TeamFeedbackPro.Front/src/main.tsx
@@ -8,3 +8,22 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+
+{/* 
+  
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+// Importação global dos CSS
+import "./index.css";
+import "./components.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+      
+*/}
